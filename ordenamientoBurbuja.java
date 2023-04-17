066d1a81c2a080c842d525cea1174f66f861832d
broken_Arm <113112221+xStarHandx@users.noreply.github.com>
2023-04-16 20:44:40 -0400

    Update ordenamientoBurbuja.java

--- ordenamientoBurbuja.java
+++ ordenamientoBurbuja.java
@@ -2,10 +2,19 @@ import java.util.Scanner;
 public class ordenamientoBurbuja {
 
 	public static void main(String[] args) {
-		
 		Scanner scan = new Scanner(System.in);
 		
 
+	}
+	
+	private static void burbuja(int[]vector){
+		for(int i=0;i<vector.lenght;i++){
+			for(int j=i+1;j<vector.lenght;j++){
+				if(vector[i]<vector[j]){
+					intercambiar(vector,i,j);
+				}
+			}
+		}
 	}
 
 }
